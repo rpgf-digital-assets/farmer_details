d5cd74472a71e68f2ae5794c92715d8f3df43a82
digital-assets-rpgf <digital.assets@rpgf.in>
2023-06-27 00:01:56 +0530

    Added delete farmer API

--- api/farmer_admin/views.py
+++ api/farmer_admin/views.py
@@ -2,8 +2,26 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from farmer.models import Farmer
+from users.models import User
+
 
 class DeleteFarmerAPIView(APIView):
     def post(self, request):
-        print("🐍 File: farmer_admin/views.py | Line: 8 | post ~ request.data",request.data)
-        return Response({'status':"success"})
+        response = {
+            'status': '',
+            'message': ''
+        }
+        try:
+            farmer_list = request.data['farmers_list']
+            print('a jfahkb fa ', type(farmer_list))
+            for farmer_pk in request.data['farmers_list']:
+                Farmer.objects.get(pk=farmer_pk).delete()
+                User.objects.get(pk=farmer_pk).delete()
+            response['status'] = 'success'
+        except Exception as e:
+            response['status'] = 'error'
+            response['message'] = str(e)
+            
+        return Response(response)
+    
