61b61669a485e6b80290a34a9345ca4f58c14c42
vishal338 <vishal@yodaplus.com>
2023-08-05 16:13:36 +0530

    fixed prod settings

--- farmer_details/prod_settings.py
+++ farmer_details/prod_settings.py
@@ -1,15 +1,23 @@
 import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = False
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('FARMER_DATABASE_NAME'),
+#         'USER': os.environ.get('FARMER_DATABASE_USER'),
+#         'PASSWORD': os.environ.get('FARMER_DATABASE_PASSWORD'),
+#         'HOST': os.environ.get('FARMER_DATABASE_HOST'),
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('FARMER_DATABASE_NAME'),
-        'USER': os.environ.get('FARMER_DATABASE_USER'),
-        'PASSWORD': os.environ.get('FARMER_DATABASE_PASSWORD'),
-        'HOST': os.environ.get('FARMER_DATABASE_HOST'),
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': f"{BASE_DIR}/db.sqlite3",
     }
 }
 
@@ -18,5 +26,5 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('FARMER_AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 PUBLIC_MEDIA_LOCATION = 'media'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-DEFAULT_FILE_STORAGE = 'nutrify.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'farmer_details.storage_backends.MediaStorage'
 
