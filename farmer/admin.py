424af181be8a84cb7aa8f20ef67d2603529bdc81
digital-assets-rpgf <digital.assets@rpgf.in>
2024-08-01 22:32:59 +0530

    added search in farmer admin model

--- farmer/admin.py
+++ farmer/admin.py
@@ -4,16 +4,21 @@ from django.contrib.admin.sites import AlreadyRegistered
 
 from farmer.models import Farmer, HistoricalFarmer
 
-app_models = apps.get_app_config('farmer').get_models()
+app_models = apps.get_app_config("farmer").get_models()
+
 
 class BaseModelAdmin(admin.ModelAdmin):
-    readonly_fields = ['is_active']
-    
+    readonly_fields = ["is_active"]
+
+
+class FarmerAdmin(admin.ModelAdmin):
+    search_fields = ["user__first_name"]
+
 
 for model in app_models:
     try:
         if model == Farmer or model == HistoricalFarmer:
-            admin.site.register(model)
+            admin.site.register(model, FarmerAdmin)
         else:
             admin.site.register(model, BaseModelAdmin)
     except AlreadyRegistered:
